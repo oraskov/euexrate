471dede801c37fb8df0795ef8d9750eb98c8c07f
Oliver Raškov <oliver.raskov@gmail.com>
2019-12-23 19:12:34 +0100

    Initial import of base file.

---
+++ getrates.py
@@ -0,0 +1,24 @@
+#! Python3
+"""Script for grabbing exchange rates from net.
+
+Use this script to web scrap data from https://www.ecb.europa.eu
+for following (implemented) currencies: CAD, CHF, DKK, GBP, NOK, SEK, USD.
+the table name in example is >>eurates<<
+"""
+
+
+__author__ = "Oliver Raskov"
+__copyright__ = "Copyright 2018"
+__credits__ = ["My family"]
+__license__ = "GPL"
+__version__ = "0.3"
+__maintainer__ = "Oliver Raskov"
+__email__ = "oliver.raskov@gmail.com"
+__status__ = "Deployment"
+
+
+def main():
+    """ The magic starts here. """
+
+if __name__ == "__main__":
+    main()
